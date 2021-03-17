fe450d929ebb5ce2021df775ba304e58501ab876
peric <peric.drazhen@gmail.com>
2021-03-17 14:01:23 +0100

    Use text instead of body

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const DEBUG = false
 addEventListener('fetch', (event) => {
   try {
     let response = handleEvent(event, require.context('./pages/', true, /\.js$/), DEBUG);
-    let newResponse = new Response(response.body, response)
+    let newResponse = new Response(response.text(), response)
 
     newResponse.headers.set("X-Frame-Options", "SAMEORIGIN");
     newResponse.headers.set("Content-Security-Policy", "frame-ancestors 'none'");
